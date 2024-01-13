bf6175aa8b6078dedb6c17cfb6ced666da9dc14b
Harsh Vardhan <114940141+Carnival02@users.noreply.github.com>
2024-01-13 17:00:58 +0000

    Conversion of DataTypes

---
+++ Basics/datatypeConversion.js
@@ -0,0 +1,17 @@
+// console.log("Harsh")
+// let harsh = "jasonhus"
+// console.log(harsh);
+let score="33"
+
+// const {score}=req.body
+console.log(typeof score);
+/* if score=33abcs and we print that than it is NaN ==> Not a number 
+*/
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+
+//"33" => 33
+//"33abs" => NaN (Not a number)
+// true => 1 ; false => 0 
+// Empty string ""=> false
+// "Hotest ,any name " => true 
