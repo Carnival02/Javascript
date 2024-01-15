9e6766a3f1e9c3cc1a85bf48aae54967e16223b7
Harsh Vardhan <carnivalking02@gmail.com>
2024-01-16 00:51:29 +0530

    String Conversion topic

---
+++ Basics/stringconversion.js
@@ -0,0 +1,24 @@
+let value   =   3
+let negvalue=-value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); power
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="Hello"
+let str2="HArsh"
+
+let str3=str1+str2;
+// console.log(str3);
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
