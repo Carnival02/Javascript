ad27968dfdcb5f6d6939239f0cd2fa7d682103de
Harsh Vardhan <carnivalking02@gmail.com>
2024-01-16 23:04:56 +0530

    All about Comparison

---
+++ Basics/comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1); //Both are true
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); //It is true because in js while comparison null treating as a 0 that's why 3rd is true and 1st is flase
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0); all are false
+
+//=== strictly check also check the data type as well
