a3a2c4dd7530f073797c9561c5f90e01b8eb633c
Harsh Vardhan <carnivalking02@gmail.com>
2024-01-27 23:05:31 +0530

    Scopes In JS

--- 03_Basics/Scope.js
+++ 03_Basics/Scope.js
@@ -12,4 +12,40 @@ if (true) {
 //console.log(a); this is not in the scope show error
 //console.log(b);
 // console.log(c);
-console.log(a);
+// console.log(a);
+
+function one(){
+
+    const username ="HArsh"
+    function two() {
+        const website="Youtube"
+       // console.log(username);
+        
+    }
+    // console.log(website); this will Not execute 
+
+     two() //this will execute 
+
+}
+// one()
+
+if (true) {
+    const username="HArhs"
+    if(username==="HArhs"){
+        const website=" Youtube"
+        // console.log(username+website);// This will run and shows HArhs Youtube
+    } 
+    // console.log(website); this will show error because Website ka scope upar khtm ho gya hia 
+}
+// console.log(username); This will show error because scope is ended upar hi 
+
+//+++++++++++++++++++++  INTERSTING *****************************
+addOne(5) //No problem while execution
+
+function addOne(num){
+    return num+1
+}
+ addTwo(5) //Show error because we are holding value of function in add two
+const addTwo =function(num){
+    return num + 2
+}
