146cbbeacfa94e306837c8076e9b4ad82aee47bf
Harsh Vardhan <carnivalking02@gmail.com>
2024-01-27 22:41:58 +0530

    Scopes in JS

---
+++ 03_Basics/Scope.js
@@ -0,0 +1,15 @@
+// var c=300
+
+let a=300
+
+if (true) {
+    let a=10
+    const b=20
+    console.log("INNER",a);
+    
+}
+
+//console.log(a); this is not in the scope show error
+//console.log(b);
+// console.log(c);
+console.log(a);
