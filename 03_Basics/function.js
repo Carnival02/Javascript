ee6e2f7373ba763fc686c30d269ea2583c92991e
Harsh Vardhan <carnivalking02@gmail.com>
2024-01-27 22:31:39 +0530

    Function Final

--- 03_Basics/function.js
+++ 03_Basics/function.js
@@ -28,9 +28,36 @@ function addTwoNumber(number1,number2) {
 // const result=addTwoNumber(5,4)
 // console.log("Result: ",result);
 
-function loginUser(username) {
-    return `${username} just logged in`
+// function loginUser(username) {
+//     return `${username} just logged in`
     
+// }
+
+// console.log(loginUser("Harsh")); // when no value is passed than It shows undefined logged in 
+
+function calculateCartPrice(...num1){
+    return num1
+}
+//console.log(calculateCartPrice(200,400,600,700));
+const user={
+    username: "Harsh",
+    price:155
 }
 
-console.log(loginUser("Harsh")); // when no value is passed than It shows undefined logged in 
+function handelObject(anyobject){
+
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+} 
+
+// handelObject(user)
+handelObject({
+    username:"Same",
+    price:399
+})
+
+const myNewArray=[200,100,300,600]
+function returnSecondValue(getArray) {
+    return getArray[1]
+    
+}
+console.log(returnSecondValue(myNewArray));
