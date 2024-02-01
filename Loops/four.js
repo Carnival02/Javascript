c5fef2caeaa060e5583098f19c091f52807effc2
Harsh Vardhan <carnivalking02@gmail.com>
2024-02-01 22:29:25 +0530

    FOR In Loop

---
+++ Loops/four.js
@@ -0,0 +1,17 @@
+const myObject={
+    js:'JavaScript',
+    cpp:'c++',
+    rb:'rb',
+    swift:'swift by apple'
+}
+
+for (const key in myObject) {
+//    console.log(`${key} for shortcut is for ${myObject[key]}`);
+}
+
+const programming =["js","ruby","cpp","python"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+    
+}
