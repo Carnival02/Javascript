42acae5531f0d14f629a8cc3738bc77dfacdf9d9
Harsh Vardhan <carnivalking02@gmail.com>
2024-03-28 21:34:42 +0530

    Object advance disscusion

---
+++ OOPs/mathpy.js
@@ -0,0 +1,27 @@
+const descriptor=Object.getOwnPropertyDescriptor(Math,"PI")
+console.log(descriptor);
+
+
+const myNewObject=Object.create(null)
+
+const chai={
+    name:'ginger Chai',
+    price:250,
+    isAvailable:true
+}
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+Object.defineProperty(chai,'name',{
+    // writable:false,
+    enumerable:false,
+
+})
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+for (let [key,value] of Object.entries(chai)) {
+
+    if(typeof value !=='function'){
+        console.log(`${key}:${value}`);
+    }
+        
+}
